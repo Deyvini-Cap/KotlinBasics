f5114df9296fad936175d8e7b4f950d089727a81
dantas <dantasdeyvini@gmail.com>
2024-09-19 12:02:05 -0500

    Modified data class

--- app/src/main/java/com/example/firsttest/sintaxis/DataClase.kt
+++ app/src/main/java/com/example/firsttest/sintaxis/DataClase.kt
@@ -1,11 +1,10 @@
 package com.example.firsttest.sintaxis
 
 fun main(){
-    /*val jose = Person("Jose", "Galdamez")
-    val yenifer = Person("Yenifer", "Rosales")
+    val jose = Person("Jose", "Galdamez", CivilStatus.Married)
+    val yenifer = Person("Yenifer", "Rosales", CivilStatus.Comprometed)
 
     println(jose.name)
     println(yenifer.name)
 
-     */
 }
